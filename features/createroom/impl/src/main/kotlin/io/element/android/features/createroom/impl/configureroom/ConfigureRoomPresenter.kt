340116f77c7f902ad17dcae3c91dc209018c8a33
Talha Asghar <iamtalhaasghar@gmail.com>
2025-02-24 17:19:02 +0500

    disable e2e in private rooms
    Because we skip device verification. It shows a warning in private room in all msgs that msg has been sent from an unverified device. 
    Hence disabling encryption in private rooms too.

--- features/createroom/impl/src/main/kotlin/io/element/android/features/createroom/impl/configureroom/ConfigureRoomPresenter.kt
+++ features/createroom/impl/src/main/kotlin/io/element/android/features/createroom/impl/configureroom/ConfigureRoomPresenter.kt
@@ -172,7 +172,9 @@ class ConfigureRoomPresenter @Inject constructor(
                 CreateRoomParameters(
                     name = config.roomName,
                     topic = config.topic,
-                    isEncrypted = config.roomVisibility is RoomVisibilityState.Private,
+                    // Because we skip device verification. It shows a warning in private room in all msgs that msg has been sent from an unverified device. 
+                    // Hence disabling encryption in private rooms too.                     
+                    isEncrypted = false, 
                     isDirect = false,
                     visibility = RoomVisibility.Private,
                     preset = RoomPreset.PRIVATE_CHAT,
