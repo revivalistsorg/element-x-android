b795661123ff8bc335b2665da1310f73d5b3d20c
Talha Asghar <iamtalhaasghar@gmail.com>
2025-07-10 14:08:45 +0500

    Update BuildTimeConfig.kt

--- plugins/src/main/kotlin/config/BuildTimeConfig.kt
+++ plugins/src/main/kotlin/config/BuildTimeConfig.kt
@@ -27,7 +27,7 @@ object BuildTimeConfig {
     val SERVICES_MAPTILER_DARK_MAPID: String? = null
     val SERVICES_POSTHOG_HOST: String? = null
     val SERVICES_POSTHOG_APIKEY: String? = null
-    val SERVICES_SENTRY_DSN: String = null
+    val SERVICES_SENTRY_DSN: String? = null
     val BUG_REPORT_URL: String = "https://rageshake.the-revivalists.org/api/submit"
     val BUG_REPORT_APP_NAME: String = "element-x-android"
 
